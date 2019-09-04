cf6b54c4fd4889322f77aa1513c8ae0ae9074a3a
Christina Douglas <chrissyidouglas@gmail.com>
2019-09-04 10:16:02 -0700

    add AddSong

---
+++ src/AddSong/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class AddSong extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      songTitle: '',
+      artist: '',
+      albumTitle: '',
+      lyrics: ''
+    
+    }
+
+  }
+  updateSong = (e) => {
+    this.setState({[e.currentTarget.name] : e.currentTarget.value})
+  }
+  render(){
+    return (
+      // <form onSubmit={this.props.addSong.bind(null, this.state)}>
+        <form>
+        <label htmlFor="title">Song:
+          <input type="text" name="title" onChange={this.updateSong} value={this.state.songTitle}/>
+        </label>
+        <label htmlFor="artist">Artist:
+          <input type="text" name="artist" onChange={this.updateSong} value={this.state.artist}/>
+        </label>
+        <label htmlFor="albumTitle">Album Title:
+          <input type="text" name="albumTitle" onChange={this.updateSong} value={this.state.albumTitle}/>
+        </label>
+        <label htmlFor="lyrics">Lyrics:
+          <input type="text" name="lyrics" onChange={this.updateSong} value={this.state.lyrics}/>
+        </label>
+        <button type='submit'>
+          Add Song
+        </button>
+      </form>
+      )
+  }
+}
+
+export default AddSong;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AddSong from './AddSong';
 
 function App() {
   return (
     <div className="App">
-    
+    <AddSong/>
     </div>
   );
 }
